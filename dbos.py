00cf9e5e66af5caec6cb0210e473c1af01185af2
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-04 02:30:04 +0300

    Update dbos.py

--- dbos.py
+++ dbos.py
@@ -6,19 +6,21 @@ import socket
 import threading
 import sys
 import os
-# Colors
-yellow='\033[92m'
-cyan='\033[92m'
-pink='\033[92m'
-green ='\033[92m'
-red ='\033[92m'
-white ='\033[92m'
-black ='\033[92m'
-blue ='\033[92m'
-# Requests
+# Reset
+Color_Off="\[\033[0m\]"       # Text Reset
+# Regular Colors
+Black="\[\033[0;30m\]"        # Black
+Red="\[\033[0;31m\]"          # Red
+Green="\[\033[0;32m\]"        # Green
+Yellow="\[\033[0;33m\]"       # Yellow
+Blue="\[\033[0;34m\]"         # Blue
+Purple="\[\033[0;35m\]"       # Purple
+Cyan="\[\033[0;36m\]"         # Cyan
+White="\[\033[0;37m\]"        # White
 
+# Requests
 os.system("clear")
-print(blue + f"З А Г Р У З К А....")
+print(Blue + f"З А Г Р У З К А....")
 time.sleep(1.5)
 os.system("clear")
 
